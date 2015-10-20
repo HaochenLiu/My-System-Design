753a4072d421eef2c6b52e685d3da8c9abfe48bc
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-19 23:53:33 -0700

    Update Singleton-lazy.cpp

--- Singleton-lazy.cpp
+++ Singleton-lazy.cpp
@@ -1,37 +1,48 @@
 #include <iostream>
-
 using namespace std;
 
-class CSingleton {
-private:
-    static CSingleton* m_pInstance;
-
-    CSingleton() {
-    }
-
-    class CGarbo {//它的唯一工作就是在析构函数中删除CSingleton的实例
-    public:
-        ~CGarbo() {
-            if(CSingleton::m_pInstance) {
-                delete CSingleton::m_pInstance;
-            }
-        }
-    };
-
-    static CGarbo Garbo;//定义一个静态成员变量，程序结束时，系统会自动调用它的析构函数
-
+class Singleton {
 public:
-    static CSingleton* GetInstance() {
-        if(m_pInstance == NULL) {//判断是否第一次调用
-            m_pInstance = new CSingleton();
-        }
-        return m_pInstance;
-    }
+	static Singleton* GetInstance() {
+		cout<<"Get instance"<<endl;
+		if (m_Instance == NULL) {
+			cout<<"Create instance"<<endl;
+			m_Instance = new Singleton();
+		}
+		return m_Instance;
+	}
+
+	static void DestoryInstance() {
+		cout<<"Destroy time"<<endl;
+		if(m_Instance != NULL ) {
+			cout<<"Delete time"<<endl;
+			delete m_Instance;
+			m_Instance = NULL;
+		}
+	}
+
+	int GetTest() {
+		return m_Test;
+	}
+private:
+	Singleton() {
+		m_Test = 10;
+	}
+	static Singleton* m_Instance;
+	int m_Test;
 };
 
+Singleton* Singleton::m_Instance = NULL;
+
 int main() {
-    int wait = 0;
+	Singleton* a = Singleton::GetInstance();
+	cout<<"a->GetTest() "<<a->GetTest()<<endl;
+	Singleton* b = Singleton::GetInstance();
+	cout<<"b->GetTest() "<<b->GetTest()<<endl;
+	Singleton::DestoryInstance();
+
+	int wait = 0;
     cin>>wait;
 
-    return 0; 
+	return 0;
 }
