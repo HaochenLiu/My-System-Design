df0f388e6af62447d14b0ce9ba525a5a47a28962
Haochen Liu <liuhaochen1221@hotmail.com>
2015-10-19 23:54:59 -0700

    Update Singleton-lazy.cpp

--- Singleton-lazy.cpp
+++ Singleton-lazy.cpp
@@ -3,46 +3,46 @@ using namespace std;
 
 class Singleton {
 public:
-	static Singleton* GetInstance() {
-		cout<<"Get instance"<<endl;
-		if (m_Instance == NULL) {
-			cout<<"Create instance"<<endl;
-			m_Instance = new Singleton();
-		}
-		return m_Instance;
-	}
+    static Singleton* GetInstance() {
+        cout<<"Get instance"<<endl;
+        if (m_Instance == NULL) {
+            cout<<"Create instance"<<endl;
+            m_Instance = new Singleton();
+        }
+        return m_Instance;
+    }
 
-	static void DestoryInstance() {
-		cout<<"Destroy time"<<endl;
-		if(m_Instance != NULL ) {
-			cout<<"Delete time"<<endl;
-			delete m_Instance;
-			m_Instance = NULL;
-		}
-	}
+    static void DestoryInstance() {
+        cout<<"Destroy time"<<endl;
+        if(m_Instance != NULL ) {
+            cout<<"Delete time"<<endl;
+            delete m_Instance;
+            m_Instance = NULL;
+        }
+    }
 
-	int GetTest() {
-		return m_Test;
-	}
+    int GetTest() {
+        return m_Test;
+    }
 private:
-	Singleton() {
-		m_Test = 10;
-	}
-	static Singleton* m_Instance;
-	int m_Test;
+    Singleton() {
+        m_Test = 10;
+    }
+    static Singleton* m_Instance;
+    int m_Test;
 };
 
 Singleton* Singleton::m_Instance = NULL;
 
 int main() {
-	Singleton* a = Singleton::GetInstance();
-	cout<<"a->GetTest() "<<a->GetTest()<<endl;
-	Singleton* b = Singleton::GetInstance();
-	cout<<"b->GetTest() "<<b->GetTest()<<endl;
-	Singleton::DestoryInstance();
+    Singleton* a = Singleton::GetInstance();
+    cout<<"a->GetTest() "<<a->GetTest()<<endl;
+    Singleton* b = Singleton::GetInstance();
+    cout<<"b->GetTest() "<<b->GetTest()<<endl;
+    Singleton::DestoryInstance();
 
-	int wait = 0;
+    int wait = 0;
     cin>>wait;
 
-	return 0;
+    return 0;
 }
